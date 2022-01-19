0385f9ab39a780d02c84a36c19fd6bfa7ab47a59
Angel Torres <jatorresdev@gmail.com>
2022-01-19 00:15:10 -0500

    feat: add responsive support for question card widget

--- lib/widgets/answers.dart
+++ lib/widgets/answers.dart
@@ -10,12 +10,14 @@ class Answers extends StatefulWidget {
     required this.isQuestionAnswered,
     required this.onChanged,
     required this.typeAnswer,
+    required this.isLayoutRow,
   }) : super(key: key);
 
   final List<String> incorrectAnswers;
   final String correctAnswer;
   final ValueChanged<bool> onChanged;
   final bool isQuestionAnswered;
+  final bool isLayoutRow;
   final TypeAnswer typeAnswer;
 
   @override
@@ -40,31 +42,51 @@ class _AnswersState extends State<Answers> {
 
   @override
   Widget build(BuildContext context) {
+    return widget.isLayoutRow ? widgetRow() : widgetColumn();
+  }
+
+  Column widgetColumn() {
     return Column(
       children: widget.incorrectAnswers.map(
         (answer) {
-          return RadioListTile(
-            title: (widget.typeAnswer == TypeAnswer.image
-                ? FadeInImage(
-                    alignment: Alignment.centerLeft,
-                    width: 30,
-                    height: 30,
-                    placeholder: const AssetImage('assets/icon-flag.png'),
-                    image: NetworkImage(answer),
-                    fit: BoxFit.contain,
-                  )
-                : Text(answer)),
-            value: answer,
-            groupValue: _selectedAnswer,
-            onChanged: (newValue) {
-              setState(() {
-                _selectedAnswer = newValue.toString();
-                widget.onChanged(widget.isQuestionAnswered);
-              });
-            },
+          return boxAnswer(answer);
+        },
+      ).toList(),
+    );
+  }
+
+  Row widgetRow() {
+    return Row(
+      children: widget.incorrectAnswers.map(
+        (answer) {
+          return Expanded(
+            child: boxAnswer(answer),
           );
         },
       ).toList(),
+    );
+  }
+
+  RadioListTile<String> boxAnswer(String answer) {
+    return RadioListTile(
+      title: (widget.typeAnswer == TypeAnswer.image
+          ? FadeInImage(
+              alignment: Alignment.centerLeft,
+              width: 30,
+              height: 30,
+              placeholder: const AssetImage('assets/icon-flag.png'),
+              image: NetworkImage(answer),
+              fit: BoxFit.contain,
+            )
+          : Text(answer)),
+      value: answer,
+      groupValue: _selectedAnswer,
+      onChanged: (newValue) {
+        setState(() {
+          _selectedAnswer = newValue.toString();
+          widget.onChanged(widget.isQuestionAnswered);
+        });
+      },
     );
   }
 }
