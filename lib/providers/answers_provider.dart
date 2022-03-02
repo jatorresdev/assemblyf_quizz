f6d542d25553b406447ffecd2c5e881698cb5694
Angel Torres <jatorresdev@gmail.com>
2022-03-01 20:07:15 -0500

    fix: adjust the scoring process of a quiz

--- lib/providers/answers_provider.dart
+++ lib/providers/answers_provider.dart
@@ -4,7 +4,6 @@ import 'package:assemblyf_quizz/notifiers/answers_notifier.dart';
 import 'package:assemblyf_quizz/models/correct_answer.dart';
 
 final answersProvider =
-    StateNotifierProvider.autoDispose<AnswersNotifier, List<CorrectAnswer>>(
-        (ref) {
+    StateNotifierProvider<AnswersNotifier, List<CorrectAnswer>>((ref) {
   return AnswersNotifier();
 });
