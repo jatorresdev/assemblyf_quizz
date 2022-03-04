e43047644c92095993d76e84f8297d938d95d80b
Angel Torres <jatorresdev@gmail.com>
2022-03-03 23:30:54 -0500

    feat: save quiz score in the firestore database

--- lib/models/quiz_score.dart
+++ lib/models/quiz_score.dart
@@ -7,12 +7,14 @@ class QuizScore {
     required this.answers,
     required this.score,
     required this.questionCount,
+    this.userIdFirebase,
   });
 
   late final Quiz quiz;
   late final List<CorrectAnswer> answers;
   late final int score;
   late final int questionCount;
+  late final String? userIdFirebase;
 
   factory QuizScore.fromMap(Map<String, dynamic> map) {
     return QuizScore(
@@ -22,13 +24,31 @@ class QuizScore {
           .toList(),
       score: map['score'] as int,
       questionCount: map['questionCount'] as int,
+      userIdFirebase: map['userIdFirebase'],
     );
   }
 
-  Map<String, dynamic> toMap() => {
-        "quiz": quiz,
-        "answers": answers,
+  Map<String, dynamic> toJson() => {
+        "quiz": quiz.toJson(),
+        "answers": answers.map((answer) => answer.toJson()).toList(),
         "score": score,
         "questionCount": questionCount,
+        if (userIdFirebase != null) "userIdFirebase": userIdFirebase,
       };
+
+  QuizScore copyWith({
+    required Quiz quiz,
+    required List<CorrectAnswer> answers,
+    required int score,
+    required int questionCount,
+    String? userIdFirebase,
+  }) {
+    return QuizScore(
+      quiz: quiz,
+      answers: answers,
+      score: score,
+      questionCount: questionCount,
+      userIdFirebase: userIdFirebase ?? this.userIdFirebase,
+    );
+  }
 }
