e9f14973b9465280d12a3efba528c88dd20c7365
Angel Torres <jatorresdev@gmail.com>
2022-03-07 22:28:37 -0500

    feat: add create repository

--- lib/repositories/quiz_repository.dart
+++ lib/repositories/quiz_repository.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:assemblyf_quizz/models/correct_answer.dart';
 import 'package:assemblyf_quizz/models/quiz.dart';
 import 'package:assemblyf_quizz/models/quiz_score.dart';
+import 'package:assemblyf_quizz/models/request_create_quiz.dart';
 import 'package:assemblyf_quizz/models/response.dart';
 import 'package:assemblyf_quizz/services/quiz_api_client.dart';
 
@@ -18,5 +19,9 @@ class QuizRepository {
 
   Future<QuizScore> getScore(int quizId, List<CorrectAnswer> answers) {
     return quizApiClient.score(quizId, jsonEncode(answers));
+  }
+
+  Future<Quiz> create(RequestCreateQuiz requestCreateQuiz) {
+    return quizApiClient.create(jsonEncode(requestCreateQuiz));
   }
 }
